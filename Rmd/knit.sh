5fad618d904eb656d1b2bf099dc38d26bf6d743a
Angus H. Wright <awright@astro.rub.de>
2022-02-08 15:33:19 +0100

    rename folders

--- Rmd/knit.sh
+++ Rmd/knit.sh
@@ -9,13 +9,18 @@ then
   echo "Error: calling syntax is ./knit.sh [section] [fmt]" 
   echo "       Possible choices:"
   echo "       -> section: {0,1a,1b,...,4c,4d}, [0..4], all"
-  echo "       -> fmt:     {html,slidy,pdf,all}"
+  echo "       -> fmt:     {html,slidy,pdf,github,all}"
   exit 1 
 fi 
 
 #Read the section and format lists 
 section_list=$1
 fmt_list=$2 
+style=$3
+if [ "$style" == "" ]
+then 
+  style="downcute"
+fi 
 
 if [ "$section_list" == "all" ] 
 then 
@@ -24,7 +29,7 @@ fi
 
 if [ "$fmt_list" == "all" ] 
 then 
-  fmt_list="html pdf slidy"
+  fmt_list="html pdf slidy github"
 fi 
 
 for section in $section_list
@@ -89,16 +94,20 @@ do
     #Check that the output format is valid {{{
     case $fmt in 
       html)
-        echo "Output Format is ${fmt}. Using downcute!"
-        fmt=rmdformats::downcute
+        echo "Output Format is ${fmt}. Using ${style}!"
+        longfmt=rmdformats::${style}
+        ;;
+      github)
+        echo "Output Format is ${fmt}"
+        longfmt=rmarkdown::github_document
         ;;
       pdf)
         echo "Output Format is ${fmt}"
-        fmt=${fmt}_document 
+        longfmt=${fmt}_document 
         ;;
       slidy)
         echo "Output Format is ${fmt}"
-        fmt=${fmt}_presentation
+        longfmt=${fmt}_presentation
         ;;
       *) 
         echo "Bad Output Format: ${fmt}"
@@ -108,16 +117,35 @@ do
     #}}}
     
     #Knit the requests Section+Chapter into the requested Format {{{
-    echo "Knitting Section ${section} to ${fmt}" 
-    R --slave --no-save > render_${section}_${fmt//rmdformats::/}.log 2>&1 <<- END
+    echo "Knitting Section ${section} to ${longfmt}" 
+    R --slave --no-save > render_${section}_${fmt}.log 2>&1 <<- END
 			library(rmarkdown); 
-			rmarkdown::render("IntroductionToStatistics_Section${section}.Rmd","${fmt}") 
+			rmarkdown::render("IntroductionToStatistics_Section${section}.Rmd","${longfmt}") 
 		END
-    if [ "${fmt}" != "pdf_document" ] 
-    then 
-      mv IntroductionToStatistics_Section${section}.html IntroductionToStatistics_Section${section}_${fmt//rmdformats::/}.html
-    fi 
     #}}}
+
+    #Move the output to the relevant place {{{
+    case $fmt in 
+      html)
+        mv IntroductionToStatistics_Section${section}.html ../HTML_page/IntroductionToStatistics_Section${section}_${fmt}.html
+        ;;
+      github)
+        mv IntroductionToStatistics_Section${section}.md ../IntroductionToStatistics_Section${section}.md
+        mv IntroductionToStatistics_Section${section}_files ../
+        ;;
+      pdf)
+        mv IntroductionToStatistics_Section${section}.pdf ../PDF/IntroductionToStatistics_Section${section}.pdf
+        ;;
+      slidy)
+        mv IntroductionToStatistics_Section${section}.html ../HTML_slides/IntroductionToStatistics_Section${section}_${fmt}.html
+        ;;
+      *) 
+        echo "Bad Output Format: ${fmt}"
+        exit 1
+        ;;
+    esac
+    #}}}
+
   done
 done
 
